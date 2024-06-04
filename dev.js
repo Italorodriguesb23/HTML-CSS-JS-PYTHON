c9957bdced2a21a1a1519bf2dcdcf566ba0751f8
Italorodriguesb23 <italorbezerra1@gmail.com>
2024-06-04 20:59:44 -0300

    CDTP

---
+++ dev.js
@@ -0,0 +1,21 @@
+function ircursos(){
+    window.location = "cursos.html"
+  }
+
+  function irinicio(){
+    window.location = "lp.html"
+  }
+
+  function irlogin(){
+    window.location = "login.html"
+  }
+
+  function irmatricula(){
+    window.location = "matricula.html"
+  }
+
+  function irlista(){
+    window.location = "lista.html"
+  }
+
+  
